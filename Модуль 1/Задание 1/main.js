41a6cfd2191897e4fc978af137aee755a18edddb
TimurKhabibulin474 <Timur.Khabibulin@urfu.me>
2024-05-04 14:09:52 +0500

    Update main.js

--- Модуль 1/Задание 1/main.js
+++ Модуль 1/Задание 1/main.js
@@ -6,7 +6,7 @@
         document.addEventListener('click', event => {
             if(event.target.classList.contains("js-open-dropdown"))
                 dropdownMenu.classList.toggle('dropdown-menu__visible');
-            else if(!event.target.classList.contains("dropdown-menu") && !event.target.classList.contains("dropdown-item"))
+            else if (!(event.target.classList.contains("dropdown-menu") || event.target.classList.contains("dropdown-item")))
                 dropdownMenu.classList.remove('dropdown-menu__visible');
         });
     });
